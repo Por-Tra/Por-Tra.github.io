a069658da4722a983469947397fa62ae1e5da9d4
Por-Tra <151206590+Por-Tra@users.noreply.github.com>
2025-11-26 15:35:31 +0100

    feat: Implement CardSwap component with animated card transitions and styling

--- React/Portfolio/src/components/Projects.jsx
+++ React/Portfolio/src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import { useTranslation } from '../i18n';
 import Navbar from './Navbar';
 import Aurora from './Aurora';
+import CardSwap, { Card } from './CardSwap';
 import './Projects.css';
 
 function Projects({ onNavigate }) {
@@ -18,7 +19,27 @@ function Projects({ onNavigate }) {
 
       {/* Content */}
       <div className="page-content">
-        {/* Votre contenu ici */}
+        <div style={{ height: '600px', position: 'relative' }}>
+          <CardSwap
+            cardDistance={60}
+            verticalDistance={70}
+            delay={5000}
+            pauseOnHover={false}
+          >
+            <Card>
+              <h3>Card 1</h3>
+              <p>Your content here</p>
+            </Card>
+            <Card>
+              <h3>Card 2</h3>
+              <p>Your content here</p>
+            </Card>
+            <Card>
+              <h3>Card 3</h3>
+              <p>Your content here</p>
+            </Card>
+          </CardSwap>
+        </div>
       </div>
 
       {/* Navbar */}
