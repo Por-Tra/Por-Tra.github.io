2c6f4034f6fd7a382d435e5680afcc1f3b55f92f
Por-Tra <151206590+Por-Tra@users.noreply.github.com>
2026-01-04 22:32:59 +0100

    Config git page

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/', // Pour GitHub Pages (repo username.github.io)
+  build: {
+    outDir: 'dist',
+  },
 })
